ee4dfc913d59c5b2f61d1bbbda52625a7371a88b
yena <yeneua.dev@gmail.com>
2025-07-13 16:22:08 +0900

    chore: prettier, eslint 설정

--- eslint.config.js
+++ eslint.config.js
@@ -2,16 +2,22 @@ import js from '@eslint/js'
 import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
+import prettierPlugin from 'eslint-plugin-prettier'
+import prettierConfig from 'eslint-config-prettier'  
 import { defineConfig, globalIgnores } from 'eslint/config'
 
 export default defineConfig([
   globalIgnores(['dist']),
   {
     files: ['**/*.{js,jsx}'],
+    plugins: {
+      prettier: prettierPlugin,
+    },
     extends: [
       js.configs.recommended,
       reactHooks.configs['recommended-latest'],
       reactRefresh.configs.vite,
+      'prettier',
     ],
     languageOptions: {
       ecmaVersion: 2020,
@@ -24,6 +30,8 @@ export default defineConfig([
     },
     rules: {
       'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
+      'prettier/prettier' : 'error',
+      'semi' : ['error', 'never'],
     },
   },
 ])
