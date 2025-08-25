d830cf6a6bc8b9f040d0acb4ed8481f5eb44c112
yena <yeneua.dev@gmail.com>
2025-08-25 21:12:08 +0900

    feat: 페이지 라우팅 설정
    - react-router-dom 라이브러리 설치

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,22 @@
+import { Route, Routes } from 'react-router-dom'
+import Login from '@/pages/Login'
+import MyPage from '@/pages/MyPage'
+import Map from '@/pages/Map'
+import Search from '@/pages/Search'
+import Community from '@/pages/Community'
+
 const App = () => {
-  return <div>App</div>
+  return (
+    <>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/mypage" element={<MyPage />} />
+        <Route path="/map" element={<Map />} />
+        <Route path="/search" element={<Search />} />
+        <Route path="/community" element={<Community />} />
+      </Routes>
+    </>
+  )
 }
 
 export default App
--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,16 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
+import { BrowserRouter } from 'react-router-dom'
+import App from '@/App'
 import GlobalStyles from '@/styles/GlobalStyles.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <>
-      <GlobalStyles />
-      <App />
+      <BrowserRouter>
+        <GlobalStyles />
+        <App />
+      </BrowserRouter>
     </>
   </StrictMode>,
 )
