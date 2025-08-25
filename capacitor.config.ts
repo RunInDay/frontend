3c85e835e2b2e9764418f1cc50f220fff66873fb
yena <yeneua.dev@gmail.com>
2025-08-25 21:18:40 +0900

    chore: capacitor 프로젝트 초기화

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.ssafy.runinday',
+  appName: 'run-in-day',
+  webDir: 'dist'
+};
+
+export default config;
