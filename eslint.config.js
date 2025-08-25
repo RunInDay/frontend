532b4d05f82c3e03aaba13f791ae045fa2d5c815
yena <yeneua.dev@gmail.com>
2025-08-25 15:05:29 +0900

    chore: 프로젝트 생성

--- eslint.config.js
+++ eslint.config.js
@@ -2,36 +2,22 @@ import js from '@eslint/js'
 import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
-import prettierPlugin from 'eslint-plugin-prettier'
-import prettierConfig from 'eslint-config-prettier'  
-import { defineConfig, globalIgnores } from 'eslint/config'
+import tseslint from 'typescript-eslint'
+import { globalIgnores } from 'eslint/config'
 
-export default defineConfig([
+export default tseslint.config([
   globalIgnores(['dist']),
   {
-    files: ['**/*.{js,jsx}'],
-    plugins: {
-      prettier: prettierPlugin,
-    },
+    files: ['**/*.{ts,tsx}'],
     extends: [
       js.configs.recommended,
+      tseslint.configs.recommended,
       reactHooks.configs['recommended-latest'],
       reactRefresh.configs.vite,
-      'prettier',
     ],
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
-      parserOptions: {
-        ecmaVersion: 'latest',
-        ecmaFeatures: { jsx: true },
-        sourceType: 'module',
-      },
-    },
-    rules: {
-      'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
-      'prettier/prettier' : 'error',
-      'semi' : ['error', 'never'],
     },
   },
 ])
